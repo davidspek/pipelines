1ee46e02f7fdb80078474bf415cf16daa0c6d3e6
DavidSpek <vanderspek.david@gmail.com>
2021-01-20 15:52:37 +0100

    change naming of base template

--- create_dependabot.sh
+++ create_dependabot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm .github/dependabot.yml
-cp .github/base_dependabot.yml .github/dependabot.yml
+cp .github/base_dependabot.yml.tmp .github/dependabot.yml
 
 for directory in $(dirname $(find . -type f -name "*ockerfile*") | uniq | cut -c2-); do
      yq eval -i ".updates += {\"package-ecosystem\":\"docker\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
